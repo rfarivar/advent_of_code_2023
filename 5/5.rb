f3f240f93f2835ee35a69f0ef34e83e9e3d303fa
rfarivar <reza.farivar@gmail.com>
2024-10-14 08:29:49 -0700

    solved second part of day 5 with binary_search and recursion

--- 5/5.rb
+++ 5/5.rb
@@ -1,3 +1,5 @@
+#require 'tty-progressbar'
+
 def read_file(filename)
   lines = File.read(filename).split("\n").map(&:strip)
   seeds = lines[0].split(":")[1].split.map(&:to_i)
@@ -19,7 +21,7 @@ def read_file(filename)
   {seeds: seeds, mappings: mappings}
 end
 
-def find_e2e_mapping(seed, almanac)
+def e2e_mapping(seed, almanac)
   category = almanac[:mappings]
   source = seed
   category.each do |mappings|
@@ -38,15 +40,198 @@ def find_e2e_mapping(seed, almanac)
   source
 end
 
-def find_mappings_all_seeds(almanac)
+def find_mappings_all_seeds_round_1(almanac)
   seeds = almanac[:seeds]
-  seeds.map { |seed| find_e2e_mapping(seed, almanac)}.min
+  seeds.map { |seed| e2e_mapping(seed, almanac)}.min
+end
+
+def find_mappings_all_seeds_round_2(almanac)
+  seeds = almanac[:seeds]
+  seeds_with_ranges = []
+  (seeds.size / 2).times do |i|
+    seeds_with_ranges << {range_begin: seeds[i*2], range_size: seeds[i*2+1]}
+  end
+  seeds = []
+
+  puts "seeds_with_ranges now has #{seeds_with_ranges.size} elements"
+  # bar1 = TTY::ProgressBar.new("Processing [:bar] :percent", total: seeds_with_ranges.size)
+  seeds_with_ranges.each do |range|
+    #   bar1.advance
+    range_array = (range[:range_begin]..range[:range_begin] + range[:range_size]).to_a
+    seeds += range_array
+  end
+
+
+  puts "seeds now has #{seeds.size} elements"
+  # bar2 = TTY::ProgressBar.new("Processing [:bar] :percent", total: seeds.size)
+
+  results = seeds.map do |seed|
+    #    bar2.advance
+    a = e2e_mapping(seed, almanac)
+    puts "mapping #{seed} ==> #{a}"
+    a
+  end
+  results.min
 end
 
 def process_round_1
   almanac = read_file('5/input')
-  result = find_mappings_all_seeds(almanac)
+  result = find_mappings_all_seeds_round_1(almanac)
+  puts "result for round 1: #{result}"
+end
+
+#brute force approach for the second round. This algorithm takes a LOOONG time
+# and can only run the test case
+def process_round_2
+  almanac = read_file('5/test')
+  result = find_mappings_all_seeds_round_2(almanac)
+  puts "result for round 1: #{result}"
+end
+
+def seeds_array_to_ranges(seeds)
+  range_array = []
+  (seeds.size / 2).times do |i|
+    range_array << {start: seeds[i*2], length: seeds[i*2+1]}
+  end
+  range_array
+end
+
+def range_splitter(range, almanac)
+  result = []
+  # base case
+  range_start_mapped = e2e_mapping(range[:start], almanac)
+  range_end_mapped = e2e_mapping(range[:start] + range[:length] - 1, almanac)
+  if range_end_mapped  == range_start_mapped + range[:length] - 1
+    result << {start: range[:start], length: range[:length], mapped: range_start_mapped}
+    return result
+  end
+  #find the first range, and then let recursion take care of the rest
+  range_end = find_first_sub_range(range, almanac)
+end
+
+def find_first_sub_range(range, almanac)
+  target = range.dup
+  target[:end] = range[:start] + range[:length] - 1
+  # target[:middle] = target[:start] + (target[:end] - target[:start]) / 2
+  # Note that target[:middle] is alwyas good to use to assign to start or end, but
+  # in the condition functions, it is calculated from start and end and updated. This works
+  # because in Ruby functions are pass by reference, so we can assign range[:middle] values inside functions
+  # and then use the value outside
+  #
+  until middle_on_target_range?(target, almanac)
+    #special case for ranges of size 2 at the end of a larger range, hence length = 2
+    if target[:end] - target[:start] == 1 && middle_inside_target_range?(target, almanac)
+      target[:middle] = target[:end]
+      break
+    end
+    if middle_inside_target_range?(target, almanac)
+      target[:start] = target[:middle]
+    elsif middle_outside_target_range?(target, almanac)
+      target[:end] = target[:middle]
+    end
+  end
+  {start: range[:start], end: target[:middle], length: target[:middle] - range[:start] + 1}
+end
+
+def found_range_boundary?(range, almanac)
+  range_start = range[:start]
+  range_end = range[:end]
+  range_distance = range_end - range_start + 1
+
+  range_start_mapped = e2e_mapping(range_start, almanac)
+  range_end_mapped =  e2e_mapping(range_end, almanac)
+  element_after_range_mapped = e2e_mapping(range_end+1, almanac)
+  range_mapped_distance = range_end_mapped - range_start_mapped
+  element_after_range_mapped_distance = element_after_range_mapped - range_start_mapped
+
+  range_distance == range_mapped_distance &&
+    range_distance + 1 !=  element_after_range_mapped_distance
+end
+
+def middle_on_target_range?(range, almanac)
+  start_value = range[:start]
+  middle_value = range[:start] + (range[:end] - range[:start]) / 2
+  range[:middle] = middle_value
+  start_to_middle_distance = middle_value - start_value
+
+  start_value_mapped = e2e_mapping(start_value, almanac)
+  middle_value_mapped =  e2e_mapping(middle_value, almanac)
+  start_to_middle_mapped_distance = middle_value_mapped - start_value_mapped
+
+  element_after_middle_mapped = e2e_mapping(middle_value + 1, almanac)
+  start_to_element_after_middle_mapped_distance = element_after_middle_mapped - start_value_mapped
+
+  start_to_middle_distance == start_to_middle_mapped_distance &&
+    start_to_middle_distance + 1 !=  start_to_element_after_middle_mapped_distance
+end
+def middle_inside_target_range?(range, almanac)
+  start_value = range[:start]
+  # middle_value = range[:middle]
+  middle_value = range[:start] + (range[:end] - range[:start]) / 2
+  range[:middle] = middle_value
+  start_to_middle_distance = middle_value - start_value
+
+  start_value_mapped = e2e_mapping(start_value, almanac)
+  middle_value_mapped =  e2e_mapping(middle_value, almanac)
+  start_to_middle_mapped_distance = middle_value_mapped - start_value_mapped
+
+  element_after_middle_mapped = e2e_mapping(middle_value + 1, almanac)
+  start_to_element_after_middle_mapped_distance = element_after_middle_mapped - start_value_mapped
+
+  start_to_middle_distance == start_to_middle_mapped_distance &&
+    start_to_middle_distance + 1 ==  start_to_element_after_middle_mapped_distance
+end
+
+def middle_outside_target_range?(range, almanac)
+  start_value = range[:start]
+  # middle_value = range[:middle]
+  middle_value = range[:start] + (range[:end] - range[:start]) / 2
+  start_to_middle_distance = middle_value - start_value
+
+  start_value_mapped = e2e_mapping(start_value, almanac)
+  middle_value_mapped =  e2e_mapping(middle_value, almanac)
+  start_to_middle_mapped_distance = middle_value_mapped - start_value_mapped
+
+  element_after_middle_mapped = e2e_mapping(middle_value + 1, almanac)
+  start_to_element_after_middle_mapped_distance = element_after_middle_mapped - start_value_mapped
+
+  start_to_middle_distance != start_to_middle_mapped_distance &&
+    start_to_middle_distance + 1 !=  start_to_element_after_middle_mapped_distance
+end
+
+def extract_sub_ranges(range, remainder = range, almanac)
+  if !remainder || remainder[:start] == range[:end] || remainder[:length] <= 0
+    return []
+  end
+  first_sub_range = find_first_sub_range(remainder, almanac)
+  remainder = {start: first_sub_range[:end]+1, length: remainder[:length] - first_sub_range[:length]}
+  partial_result = extract_sub_ranges(range, remainder, almanac)
+  partial_result << first_sub_range
+end
+
+def find_lowest_location_number(almanac)
+  seed_ranges = seeds_array_to_ranges(almanac[:seeds])
+  all_sub_ranges = seed_ranges.flat_map do |range|
+    extract_sub_ranges(range, almanac)
+  end
+  location_values_for_first_element_of_all_sub_ranges = all_sub_ranges.map do |sub_range|
+    e2e_mapping(sub_range[:start], almanac)
+  end
+  location_values_for_first_element_of_all_sub_ranges.min
+end
+
+# This approach takes a total of 0.5 seconds to run the large input, as compared to
+# 10+ hours that the brtue force approach took (and didn't finish!)
+# Here, we extract the sub_ranges of each range where they map to contingous end-to-end
+# location ranges. The first sub_range is found using binary search, and then use recursion
+# to find all of sub_ranges. Finally, we flat_map all the subranges and run the e2e function on
+# the first element of each subrange. For reference, 10 seed ranges results into
+# 149 contigous sub_ranges. The sum total of all subranges would have been 2_398_198_298
+def process_round_2_using_log_n_approach
+  almanac = read_file('5/input')
+  result = find_lowest_location_number(almanac)
   puts "result for round 1: #{result}"
 end
 
 process_round_1
+process_round_2_using_log_n_approach
--- 5/test_run.rb
+++ 5/test_run.rb
@@ -4,30 +4,130 @@ require_relative './5'
 class TestRun < Minitest::Test
   def setup
     super
+    @almanac= read_file("5/test")
   end
 
   def test_file_read
-    almanac = read_file("5/test")
-    assert almanac.class == Hash
-    assert_equal [79, 14, 55, 13], almanac[:seeds]
-    assert_equal 7, almanac[:mappings].size
-    assert_equal [50, 98, 2], almanac[:mappings][0][0]
-    assert_equal [52, 50, 48], almanac[:mappings][0][1]
-    assert_equal [0, 11, 42], almanac[:mappings][2][1]
-    assert_equal [56, 93, 4], almanac[:mappings][6][1]
+    assert @almanac.class == Hash
+    assert_equal [79, 14, 55, 13], @almanac[:seeds]
+    assert_equal 7, @almanac[:mappings].size
+    assert_equal [50, 98, 2], @almanac[:mappings][0][0]
+    assert_equal [52, 50, 48], @almanac[:mappings][0][1]
+    assert_equal [0, 11, 42], @almanac[:mappings][2][1]
+    assert_equal [56, 93, 4], @almanac[:mappings][6][1]
   end
 
-  def test_find_e2e_mapping
-    almanac = read_file("5/test")
-    assert_equal 82, find_e2e_mapping(79, almanac)
-    assert_equal 43, find_e2e_mapping(14, almanac)
-    assert_equal 86, find_e2e_mapping(55, almanac)
-    assert_equal 35, find_e2e_mapping(13, almanac)
+  def test_e2e_mapping
+    assert_equal 82, e2e_mapping(79, @almanac)
+    assert_equal 43, e2e_mapping(14, @almanac)
+    assert_equal 86, e2e_mapping(55, @almanac)
+    assert_equal 35, e2e_mapping(13, @almanac)
   end
 
-  def test_find_mappings_all_seeds
-    almanac = read_file("5/test")
-    assert_equal 35, find_mappings_all_seeds(almanac)
+  def test_find_mappings_all_seeds_round_
+    assert_equal 35, find_mappings_all_seeds_round_1(@almanac)
   end
 
+  # def test_find_mappings_all_seeds_round_2
+  #   almanac = read_file("5/test")
+  #   assert_equal 46, find_mappings_all_seeds_round_2(almanac)
+  # end
+
+  def test_seeds_array_to_ranges
+    assert_equal [
+                   {start: 79, length:14},
+                   {start: 55, length:13}],
+                 seeds_array_to_ranges(@almanac[:seeds])
+  end
+
+  def test_find_range_end
+    range_first_0 = seeds_array_to_ranges(@almanac[:seeds])[0]
+    expected_first_0_range = {start: 79, end: 81, length: 3}
+    assert_equal expected_first_0_range, find_first_sub_range(range_first_0, @almanac)
+
+    range_first_1 = {start:82, length: 11}
+    expected_first_1_range = {start: 82, end: 91, length: 10}
+    assert_equal expected_first_1_range, find_first_sub_range(range_first_1, @almanac)
+
+    range_first_2 = {start:92, length: 1}
+    expected_first_2_range = {start: 92, end: 92, length: 1}
+    assert_equal expected_first_2_range, find_first_sub_range(range_first_2, @almanac)
+
+    range_second_0 = seeds_array_to_ranges(@almanac[:seeds])[1]
+    expected_first_range = {start: 55, end: 58, length: 4}
+    assert_equal expected_first_range, find_first_sub_range(range_second_0, @almanac)
+
+    range_second_1 = {start:59, length: 9}
+    expected_second_1_range = {start: 59, end: 61, length: 3}
+    assert_equal expected_second_1_range, find_first_sub_range(range_second_1, @almanac)
+
+    range_second_2 = {start:62, length: 6}
+    expected_second_2_range = {start: 62, end: 65, length: 4}
+    assert_equal expected_second_2_range, find_first_sub_range(range_second_2, @almanac)
+
+    range_second_3 = {start:66, length: 2}
+    expected_second_3_range = {start: 66, end: 67, length: 2}
+    assert_equal expected_second_3_range, find_first_sub_range(range_second_3, @almanac)
+  end
+
+  def test_extract_sub_ranges
+    range_first = seeds_array_to_ranges(@almanac[:seeds])[0]
+    range_second = seeds_array_to_ranges(@almanac[:seeds])[1]
+
+    expected_ranges =  [
+      {start: 79, length: 3, end: 81},
+      {start: 82, length: 10, end: 91},
+      {start: 92, length: 1, end: 92}
+    ]
+    assert_equal expected_ranges, extract_sub_ranges(range_first, @almanac).reverse
+
+    expected_ranges =  [
+      {start: 55, length: 4, end: 58},
+      {start: 59, length: 3, end: 61},
+      {start: 62, length: 4, end: 65},
+      {start: 66, length: 2, end: 67}
+    ]
+    assert_equal expected_ranges, extract_sub_ranges(range_second, @almanac).reverse
+  end
+
+  def test_find_lowest_location_number
+    assert_equal 46, find_lowest_location_number(@almanac)
+  end
 end
+
+# mapping 79 ==> 82
+# mapping 80 ==> 83
+# mapping 81 ==> 84
+#
+# mapping 82 ==> 46
+# mapping 83 ==> 47
+# mapping 84 ==> 48
+# mapping 85 ==> 49
+# mapping 86 ==> 50
+# mapping 87 ==> 51
+# mapping 88 ==> 52
+# mapping 89 ==> 53
+# mapping 90 ==> 54
+# mapping 91 ==> 55
+#
+# mapping 92 ==> 60
+######### mapping 93 ==> 68
+#
+# ========================
+# mapping 55 ==> 86
+# mapping 56 ==> 87
+# mapping 57 ==> 88
+# mapping 58 ==> 89
+#
+# mapping 59 ==> 94
+# mapping 60 ==> 95
+# mapping 61 ==> 96
+#
+# mapping 62 ==> 56
+# mapping 63 ==> 57
+# mapping 64 ==> 58
+# mapping 65 ==> 59
+#
+# mapping 66 ==> 97
+# mapping 67 ==> 98
+######### mapping 68 ==> 99

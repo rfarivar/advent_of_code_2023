8a1b7ea18c722df514f61049ecfa1ce7aa70d427
rfarivar <reza.farivar@gmail.com>
2024-10-21 20:56:41 -0700

    solved day 10 part 2

--- 10/pipe_maze.rb
+++ 10/pipe_maze.rb
@@ -7,6 +7,10 @@ class PipeMaze
     @maze = File.read(filename).split("\n").map(&:strip).map(&:chars)
     @maze_num_rows = @maze.size
     @maze_num_cols = @maze[0].size
+    @maze_pipes = []
+    maze_pipes_row = [false] * @maze_num_cols
+    @maze_num_rows.times {@maze_pipes << maze_pipes_row.dup}
+    @maze_borders_found = false
   end
 
   def maze_at(location)
@@ -43,9 +47,26 @@ class PipeMaze
       connected << south if ['|', 'L', 'J'].include?(maze_at(south))
       connected << east if ['-', '7', 'J'].include?(maze_at(east))
       connected << west if ['-', 'L', 'F'].include?(maze_at(west))
+      replace_s_with_real_character(connected, north, south, east, west, row, col)
       connected
     else
        []
+    end
+  end
+
+  def replace_s_with_real_character(connected, north, south, east, west, row, col)
+    if connected.include?(north) && connected.include?(south)
+      @maze[row][col] = '|'
+    elsif connected.include?(north) && connected.include?(east)
+      @maze[row][col] = 'L'
+    elsif connected.include?(north) && connected.include?(west)
+      @maze[row][col] = 'J'
+    elsif connected.include?(south) && connected.include?(east)
+      @maze[row][col] = 'F'
+    elsif connected.include?(south) && connected.include?(west)
+      @maze[row][col] = '7'
+    elsif connected.include?(east) && connected.include?(west)
+      @maze[row][col] = '-'
     end
   end
 
@@ -55,6 +76,7 @@ class PipeMaze
     next_location = connected_locations(start_location).first
     location = start_location
     while location != start_location  || loop_length <= 0
+      set_maze_pipes(location)
       previous_location = location
       location = next_location
       connected_locations_to_current = connected_locations(location)
@@ -62,6 +84,7 @@ class PipeMaze
       next_location = connected_locations_to_current.first
       loop_length += 1
     end
+    @maze_borders_found = true
     loop_length
   end
 
@@ -75,8 +98,74 @@ class PipeMaze
     end
     []
   end
+
+  def set_maze_pipes(location)
+    row, col = location
+    @maze_pipes[row][col] = true
+  end
+
+  def maze_pipe?(location)
+    row, col = location
+    @maze_pipes[row][col]
+  end
+
+  def count_vertical_borders(row_above_index, column, before_or_after)
+    find_loop_length unless @maze_borders_found
+    row_below_index = row_above_index + 1
+    columns = @maze[0].size
+
+    if before_or_after == :before
+      beginning_column = 0
+      ending_column = [0, column - 1].max
+    else
+      beginning_column = [columns - 1, column + 1].min
+      ending_column = columns-1
+    end
+    count_vertical_borders = 0
+    (beginning_column..ending_column).each do |i|
+      a = @maze[row_above_index][i]
+      b = @maze[row_below_index][i]
+      if @maze_pipes[row_above_index][i] && (%w(F | 7).include? a) &&
+        @maze_pipes[row_below_index][i] && (%w(L | J).include? b)
+        count_vertical_borders += 1
+      end
+    end
+    count_vertical_borders
+  end
+
+  def count_inside_for_one_row(row_index)
+    find_loop_length unless @maze_borders_found
+    columns = @maze[0].size
+    row = row_index
+
+    inside = 0
+    (1...columns-1).each do |col|
+      unless @maze_pipes[row][col] # only count if false, aka not a border cell
+        vertical_borders_before = count_vertical_borders(row, col, :before)
+        vertical_borders_after = count_vertical_borders(row, col, :after)
+        if [vertical_borders_before.odd?, vertical_borders_after.odd?].all?
+          inside += 1
+        end
+      end
+    end
+    inside
+  end
+
+  def count_all_inside
+    inside = 0
+    (1...@maze_num_rows - 1).each do |row|
+      inside += count_inside_for_one_row(row)
+    end
+    inside
+  end
 end
 
 maze = PipeMaze.new("10/input")
-l = maze.find_loop_length
-puts "Maze loop length is #{l}, therefor half-way point is: #{l/2}"
+loop_length = maze.find_loop_length
+puts "Maze loop length is #{loop_length}, therefor half-way point is: #{loop_length/2}"
+
+# The main insight for part 2 is that for any point that is inside the contour, the number of
+# vertical borders before and after that point in the same row are odd. This would also be true for
+# horizontal borders above and below, but checking that is unnecessary.
+count_all_inside = maze.count_all_inside
+puts "Maze has #{count_all_inside} locations inside"
--- 10/test_pipe_maze.rb
+++ 10/test_pipe_maze.rb
@@ -7,6 +7,11 @@ class TestPipeMaze < Minitest::Test
     @test_maze_1 = PipeMaze.new("10/test1")
     @test_maze_2 = PipeMaze.new("10/test2")
     @test_maze_3 = PipeMaze.new("10/test3")
+
+    @test_maze_4 = PipeMaze.new("10/test4")
+    @test_maze_5 = PipeMaze.new("10/test5")
+    @test_maze_6 = PipeMaze.new("10/test6")
+    @test_maze_7 = PipeMaze.new("10/test7")
   end
 
   def test_load_maze
@@ -42,4 +47,28 @@ class TestPipeMaze < Minitest::Test
     a.difference(b).any? || b.difference(a).any?
   end
 
+  def test_count_vertical_borders
+    row_above = @test_maze_4.maze[5]
+    row_below = @test_maze_4.maze[6]
+    # .|L-7.F-J|.
+    # .|..|.|..|.
+    expected_before = [0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4]
+    expected_after =  [4, 3, 3, 3, 2, 2, 1, 1, 1, 0, 0]
+    (0..10).each do |i|
+      assert_equal expected_before[i], @test_maze_4.count_vertical_borders(5, i, :before), "index: #{i}"
+      assert_equal expected_after[i], @test_maze_4.count_vertical_borders(6, i, :after), "index: #{i}"
+    end
+  end
+
+  def test_count_inside
+    assert_equal 4, @test_maze_4.count_inside_for_one_row(6)
+  end
+
+  def test_count_all_inside
+    assert_equal 4, @test_maze_4.count_all_inside
+    assert_equal 4, @test_maze_5.count_all_inside
+    assert_equal 8, @test_maze_6.count_all_inside
+    assert_equal 10, @test_maze_7.count_all_inside
+
+  end
 end

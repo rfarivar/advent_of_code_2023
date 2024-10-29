fd0f5e9d9acb98e295f6e4c4021e226a0895da05
rfarivar <reza.farivar@gmail.com>
2024-10-28 19:17:53 -0700

    solved part 2 of day 12

--- 12/12.rb
+++ 12/12.rb
@@ -1,7 +1,8 @@
 class HotSprings
-  attr_accessor :spring_rows
+  attr_accessor :spring_rows, :dp_cache
   def initialize(filename)
     read_file(filename)
+    @dp_cache = {}
   end
 
   def read_file(filename)
@@ -20,10 +21,15 @@ class HotSprings
 
   def count_recursive(row_string, preceding_char, input_groups)
     # p("#{preceding_char + row_string}", input_groups)
+    if @dp_cache.include?([row_string, preceding_char, input_groups])
+      return @dp_cache[[row_string, preceding_char, input_groups]]
+    end
     groups = input_groups.dup
     if row_string.empty? && (groups.empty? || (groups.size == 1 && groups.first == 0))
+      @dp_cache[[row_string, preceding_char, input_groups]] = 1
       return 1
     elsif row_string.empty? && (!groups.empty? ) # && groups.first != 0
+      @dp_cache[[row_string, preceding_char, input_groups]] = 0
       return 0
     end
 
@@ -33,54 +39,64 @@ class HotSprings
         # We have a #. means a sequence has come to an end.
         # We should check to see if it has used up all the group budget. if not, we have an error
         # condition, which means a bad choice was made previously
+        @dp_cache[[row_string, preceding_char, input_groups]] = 0
         return 0
       elsif preceding_char == '#' && groups.first == 0
         # We have a #. means a sequence has come to an end, and the budget has also finished.
         # We're good to continue
         groups.shift(1)
-        return count_recursive(row_string[1..], '.' , groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '.' , groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       elsif preceding_char == '.'
         # we have a .. just continue
-        return count_recursive(row_string[1..], '.' , groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '.' , groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       end
     elsif first_char == '#'
       if preceding_char == '#' && groups.first == 0
         # We have a ## means we are in a continuing sequence
         # We should check to see if it has used up all the group budget. if so, we have an error
         # condition, which means a bad choice was made previously
+        @dp_cache[[row_string, preceding_char, input_groups]] = 0
         return 0
       elsif preceding_char == '#' && groups.first > 0
         # We have a ## means we are in a continuing sequence, and we still have budget for the
         # first_char to be #. We're good to continue
         groups[0] -= 1
-        return count_recursive(row_string[1..], '#' , groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '#' , groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       elsif preceding_char == '.' && (groups.first == 0 || groups.empty?)
         # we have a .# which is the beginning of a sequence. But if we don't have a budget, it was an error
+        @dp_cache[[row_string, preceding_char, input_groups]] = 0
         return 0
       elsif preceding_char == '.' && groups.first > 0
         # we have a .# which is the beginning of a sequence, and have the budget, so we're good to continue.
         groups[0] -= 1
-        return count_recursive(row_string[1..], '#' , groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '#' , groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       end
     elsif first_char == '?'
       if preceding_char == '#' && groups.first == 0
         # we have #?  and we had spent all the # budget before, so the sequence of # has come to an end
         # therefore ? should be a .
         groups.shift(1)
-        return count_recursive(row_string[1..], '.',  groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '.',  groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       elsif preceding_char == '#' && groups.first > 0
         # we have #?  and we still have budget for # so this character has to be a # as well.
         groups[0] -= 1
-        return count_recursive(row_string[1..], '#',  groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '#',  groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       elsif preceding_char == '.' && (groups.empty? || groups.first == 0)
         # we have .? but the budget for first group shows 0 (or there is no budget as it was legitimately used before).
-        count_recursive(row_string[1..], '.',  groups)
-        # return 0
+        @dp_cache[[row_string, preceding_char, input_groups]] = count_recursive(row_string[1..], '.',  groups)
+        return @dp_cache[[row_string, preceding_char, input_groups]]
       elsif preceding_char == '.' && groups.first > 0
         # we have .? and also have budget to make this ? a .#  But we could also choose to make it a ..
         a = count_recursive(row_string[1..], '.',  groups)
         groups[0] -= 1
         b = count_recursive(row_string[1..], '#',  groups)
+        @dp_cache[[row_string, preceding_char, input_groups]] = a + b
         return a + b
       end
     end
@@ -97,7 +113,23 @@ class HotSprings
   end
 end
 
-springs = HotSprings.new("12/input")
-puts "sum of all counts = #{springs.sum_all_counts}"
+def unfold_file(filename)
+  spring_rows = File.read(filename).split("\n").map(&:strip)
+  File.open("#{filename}_unfolded", "w") do |f|
+    spring_rows.map do |row|
+      row_string, groups = row.split
+      unfolded_groups = ([groups] * 5).join(",")
+      unfolded_row_string = ([row_string] * 5).join("?")
+      f.write("#{unfolded_row_string} #{unfolded_groups}\n")
+    end
+  end
+end
+
+# springs = HotSprings.new("12/input")
+# puts "sum of all counts = #{springs.sum_all_counts}"
+
+unfold_file("12/input")
+springs = HotSprings.new("12/input_unfolded")
+puts "sum of all counts = #{springs.sum_all_counts}, number of dp_cach entries = #{springs.dp_cache.size}"
 
 
--- 12/test_12.rb
+++ 12/test_12.rb
@@ -5,6 +5,9 @@ class Test12 < Minitest::Test
   def setup
     super
     @springs = HotSprings.new("12/test")
+    @springs_2 = HotSprings.new("12/test2")
+    unfold_file("12/test")
+    @springs_unfolded = HotSprings.new("12/test_unfolded")
   end
 
   def test_read
@@ -19,10 +22,19 @@ class Test12 < Minitest::Test
     assert_equal 1, @springs.count_possible(3)
     assert_equal 4, @springs.count_possible(4)
     assert_equal 10, @springs.count_possible(5)
-    assert_equal 7, @springs.count_possible(6)
+    # assert_equal 7, @springs.count_possible(6)
+
+    assert_equal 1, @springs_unfolded.count_possible(0)
+    assert_equal 16384, @springs_unfolded.count_possible(1)
+    assert_equal 1, @springs_unfolded.count_possible(2)
+    assert_equal 16, @springs_unfolded.count_possible(3)
+    assert_equal 2500, @springs_unfolded.count_possible(4)
+    assert_equal 506250, @springs_unfolded.count_possible(5)
   end
 
   def test_sum_all_counts
-    assert_equal 28, @springs.sum_all_counts
+    assert_equal 21, @springs.sum_all_counts
+    assert_equal 525152, @springs_unfolded.sum_all_counts
+
   end
 end
